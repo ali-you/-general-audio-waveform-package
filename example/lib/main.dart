64807c0e6fe7d628776f9568826ee3069038a5dd
aliyou <a_yousefi78@yahoo.com>
2023-07-10 10:11:46 +0330

    merged decoders

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,11 @@
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:general_audio_waveforms/src/waveforms/pulse_waveform/pulse_waveform.dart';
 import 'package:general_audio_waveforms/src/data/scaling/average_algorithm.dart';
 import 'package:general_audio_waveforms/src/data/scaling/median_algorithm.dart';
-import 'package:general_audio_waveforms/src/data/decoder/mp3_decoder.dart';
+import 'package:general_audio_waveforms/src/data/decoder/decoder.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 void main() {
@@ -40,17 +42,17 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   Duration elapsedTime = const Duration(seconds: 0);
   List<double> samples =  [];
-  String path = "/storage/emulated/0/Download/file_example_MP3_700KB.mp3";
+  // String path = "/storage/emulated/0/Download/file_example_MP3_700KB.mp3";
   // String path = "/storage/emulated/0/Download/file_example_OOG_1MB.ogg";
+  String path = "/storage/emulated/0/Download/sample-1.m4a";
 
   @override
   void initState()  {
-    // TODO: implement initState
-    // Timer.periodic(const Duration(seconds: 1), (timer) {
-    //   setState(() {
-    //     // elapsedTime += const Duration(seconds: 1);
-    //   });
-    // });
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        elapsedTime += const Duration(seconds: 1);
+      });
+    });
     super.initState();
   }
 
@@ -66,50 +68,21 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     var avgSamples = AverageAlgorithm(samples: samples,maxSample: 50).execute();
-    var medianSamples = MedianAlgorithm(samples: samples,maxSample: 50).execute();
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Flexible(child: IconButton(onPressed: _onPressed , icon: const Icon(Icons.add))),
-            // PulseWaveform(
-            //   height: 50,
-            //   width: MediaQuery.of(context).size.width ,
-            //   maxDuration: const Duration(seconds: 100),
-            //   elapsedDuration: elapsedTime,
-            //   activeColor: Colors.redAccent,
-            //   borderWidth: 2,
-            //   inactiveColor: Colors.black,
-            //   samples: samples
-            // ),
-            //
-            Flexible(
-              child: Stack(
-                children: [
-                  PulseWaveform(
-                      height: 50,
-                      width: MediaQuery.of(context).size.width * 0.43,
-                      maxDuration: const Duration(seconds: 100),
-                      elapsedDuration: elapsedTime,
-                      activeColor: Colors.redAccent,
-                      borderWidth: 0,
-                      inactiveColor: Colors.blue.withOpacity(0.5),
-                      samples: avgSamples
-                  ),
-
-                  PulseWaveform(
-                      height: 50,
-                      width: MediaQuery.of(context).size.width * 0.43,
-                      maxDuration: const Duration(seconds: 100),
-                      elapsedDuration: elapsedTime,
-                      activeColor: Colors.redAccent,
-                      borderWidth: 0,
-                      inactiveColor: Colors.yellow.withOpacity(0.5),
-                      samples: medianSamples
-                  ),
-                ],
-              ),
+            PulseWaveform(
+              height: 50,
+              width: MediaQuery.of(context).size.width ,
+              maxDuration: const Duration(seconds: 2000),
+              elapsedDuration: elapsedTime,
+              activeColor: Colors.redAccent,
+              borderWidth: 2,
+              inactiveColor: Colors.black,
+              samples: avgSamples
             ),
           ],
         ),
@@ -119,7 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<void> _onPressed() async {
     requestPermissions();
-    var s = await MP3Decoder(path:path).extract();
+    var s = await Decoder(path:path).extract();
     setState(() {
       samples = s;
     });
--- lib/src/data/decoder/decoder.dart
+++ lib/src/data/decoder/decoder.dart
@@ -1,7 +1,15 @@
 
+
+import 'dart:io';
+
+import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter/ffmpeg_session.dart';
+import 'package:ffmpeg_kit_flutter/return_code.dart';
+import 'package:wav/wav.dart';
+
 ///
 ///
-/// [Decoder] is the base Strategy . To Use each extraction you can add an object of your desired Strategy of below options.
+/// [Decoder]
 ///
 /// {@tool snippet}
 /// Example :
@@ -9,12 +17,37 @@
 ///   //TODO
 ///```
 /// {@end-tool}
-abstract class Decoder {
+ class Decoder {
 
   /// Local Path
   final String path;
 
   Decoder({required this.path});
 
-  Future<List<double>> extract() ;
+
+  @override
+  Future<List<double>> extract() async {
+    String wavPath = path.replaceRange(path.length - 3, path.length, "wav");
+    bool fileExist = await File(wavPath).exists();
+    bool canGetWave = fileExist;
+
+    if (!fileExist) {
+      FFmpegSession session = await FFmpegKit.executeAsync(
+          '-i $path -vn -acodec pcm_s16le -ar 44100 -ac 2 $wavPath');
+      canGetWave = ReturnCode.isSuccess(await session.getReturnCode());
+      if (!canGetWave) {
+        throw "Error: ${await session.getLogsAsString()}";
+      }
+    }
+
+    ///creating the wave
+    if (canGetWave) {
+      Wav wav = await Wav.readFile(wavPath);
+      return (wav.channels.toList())
+          .expand((element) => element)
+          .map((e) => e.toDouble())
+          .toList();
+    }
+    return [];
+  }
 }
